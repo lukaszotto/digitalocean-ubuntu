0e2400a82242547d6f47a405957707a4a422216e
lukasz otto <lukasz.otto@yahoo.com>
2024-10-14 14:18:36 +0200

    add database loging

--- index-ws.js
+++ index-ws.js
@@ -12,12 +12,20 @@ server.on('request', app)
 server.listen(3000, () => {
     console.log('server started on 3000')
 })
-
-// websockets
-
 const WebSocketServer = ws.Server
 
 const wss = new WebSocketServer({ server: server })
+
+process.on('SIGINT', () => {
+    wss.clients.forEach((client) => {
+        client.close()
+    })
+    server.close(() => {
+        shutdownDB()
+    })
+})
+
+// websockets
 
 wss.on('connection', (ws) => {
     const numclients = wss.clients.size
@@ -27,6 +35,11 @@ wss.on('connection', (ws) => {
     if (ws.readyState == ws.OPEN) {
         ws.send('welcome')
     }
+
+    db.run(`
+      INSERT INTO visitors (count, time)
+      VALUES (${numclients}, datetime('now'))
+      `)
 
     ws.on('close', () => {
         wss.broadcast(`Current visitors: ${numclients}`)
@@ -38,4 +51,34 @@ wss.broadcast = (data) => {
     wss.clients.forEach((client) => {
         client.send(data)
     })
+}
+
+// end websocket
+
+// begin database
+
+const sqlite = require('sqlite3')
+
+const db = new sqlite.Database(':memory:')
+
+db.serialize(() => {
+    db.run(`
+    CREATE TABLE visitors (
+      count INTEGER,
+      time TEXT
+      
+    )
+    `)
+})
+
+const getCounts = () => {
+    db.each('SELECT * FROM visitors', (err, row) => {
+        console.log(row)
+    })
+}
+
+const shutdownDB = () => {
+    getCounts()
+    console.log('shutting down db')
+    db.close()
 }
